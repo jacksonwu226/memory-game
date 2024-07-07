02b22e9b8750f059af1efa2b07d4e870a9e3868a
Jackson Wu <jacksonwu226@gmail.com>
2024-07-06 21:54:22 -0700

    changed to fragment container'

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,14 @@
-import '.../styles/header.css';
+import '../styles/header.css';
 
 export default function Header({score, bestScore}){
   return(
-    <header>
+    <>
       <h1 className='title'>Memory Card</h1>
       <p>Click on all the pokemons only once!</p>
       <div className="score-board">
         <p className="score">Score: {score}</p>
         <p className="best-score">Highest Score: {bestScore}</p>
       </div>
-    </header>
+    </>
   )
 }
